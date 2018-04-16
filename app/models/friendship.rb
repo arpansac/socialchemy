be8cec58a6aeef867f0f3510e671be367c967e88
arpansac <arpansac@gmail.com>
2018-04-16 14:32:16 +0530

    make self referential relationship for friendship between user to user

---
+++ app/models/friendship.rb
@@ -0,0 +1,8 @@
+class Friendship < ActiveRecord::Base
+
+	belongs_to :user
+
+
+	
+	belongs_to :friend, class_name: 'User'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407105659) do
+ActiveRecord::Schema.define(version: 20180415121833) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20180407105659) do
     t.integer  "parent_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
